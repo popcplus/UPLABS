43ed5459bb72b179686ebfa1cd1df5de6d745498
Kalvin Wei <kalvinwei007@gmail.com>
2023-01-27 20:14:12 +1300

    Fix the site base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://popcplus.github.io/UPLABS',
+  site: 'https://popcplus.github.io/',
   integrations: [react(), tailwind()]
 });
