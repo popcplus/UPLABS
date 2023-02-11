ddffd75acdc02b8ebdaac6873cd0f69b4fdf2dd9
Kalvin Wei <kalvinwei007@gmail.com>
2023-02-11 17:56:59 +1300

    Fix tailwind issue, upload new logo design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors')
+const sansFontFamily = require('tailwindcss/defaultTheme').fontFamily.sans;
 
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				'sans': ['"Work Sans"', sansFontFamily]
+			},
 			colors: {
 				primary: {
 					DEFAULT: colors.blue[700],
